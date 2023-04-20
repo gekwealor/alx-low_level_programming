7fef7b7a8aab5df5e7f96a87d59b7c5b678d4143
gekwealor <gekwealor@gmail.com>
2023-04-20 10:04:40 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main -Entry point
  *
  * Return: 0 always(Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
