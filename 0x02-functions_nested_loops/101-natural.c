a7f94db35ace7ee123eef5e1498a0a02ef1aa475
gekwealor <gekwealor@gmail.com>
2023-04-25 18:02:15 -0700

    Nature made the natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,17 +4,21 @@
  * main - Prints the sum of all multiples of 3 and 5 up to 1024
  * Return: Always (Success)
  */
+
 int main(void)
+
 {
-        int i = 0, z = 0;
-        while (i < 1024)
-        {
+
+	int i = 0, z = 0;
+	while (i < 1024)
+	{	
 	if ((i % 3 == 0) || (i % 5 == 0))
-	{
+	{	
 	z += i;
-        }
+	}
 	i++;
-        }
-        printf("%d\n", z);
-        return (0);
+	}
+	printf("%d\n", z);
+	return (0);
+
 }
