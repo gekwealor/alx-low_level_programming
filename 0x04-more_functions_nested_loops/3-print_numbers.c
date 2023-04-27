6ef1c0efdc68f6ebc86b4d6e124ec80bb0dba2f4
gekwealor <gekwealor@gmail.com>
2023-04-27 09:12:57 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers between 0 to 9 
+ * return: void
+ */
+
+void print_numbers(void)
+
+{
+	char c;
+
+	for (c = 48; c <= 58; c++)
+        {
+		_putchar(c);
+        }
+        _putchar('\n');
+}
