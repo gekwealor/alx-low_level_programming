8eb4d1bedcafc38751c2e5f1ecf2efa78d54c3ba
gekwealor <gekwealor@gmail.com>
2023-04-27 07:52:18 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ * Return: 1 to uppercase letter 0 for anything else  
+ */
+
+int _isupper(int x)
+{
+        if (X >= 65 && x <= 90)
+	{
+        return (1);
+ 	}
+        return (0);
+}
+
