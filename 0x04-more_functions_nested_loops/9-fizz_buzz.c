e3500aca86c4e7de62f4d1a0f7d8de83614c4d25
gekwealor <gekwealor@gmail.com>
2023-04-27 11:50:48 -0700

    Fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,27 +16,17 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 	if (i % 3 == 0 && i % 5 != 0)
-	}
-	printf(" Fizz");
-	{
+	printf("Fizz ");
 	else if (i % 5 == 0 && i % 3 != 0)
-	}
-	printf(" Buzz");
-	{
+	printf("Buzz ");
 	else if (i % 3 == 0 && i % 5 == 0)
-	}
-	printf(" FizzBuzz");
-	{
-	else if (i == 1)
-	}
-	printf("%d", i);
-	{
+	printf("FizzBuzz ");
 	else
+	printf("%d ", i);
 	}
-	printf(" %d", i);
-	}
-	}
+
 	printf("\n");
 
 	return (0);
 }
+
