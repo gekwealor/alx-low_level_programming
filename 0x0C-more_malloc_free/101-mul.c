bc0eff97286998b47a5926aa92cfd5cb1ad7e5d2
gekwealor <gekwealor@gmail.com>
2023-05-18 07:30:17 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,31 +9,29 @@
  *
  * Return: no return.
  */
+
 void _is_zero(char *argv[])
 {
-	int g, isn1 = 1, isn2 = 1;
+	int i, isn1 = 1, isn2 = 1;
 
-	for (g = 0; argv[1][g]; g++)
-		if (argv[1][g] != '0')
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
 		{
 			isn1 = 0;
 			break;
 		}
-
-	for (g = 0; argv[2][g]; g++)
-		if (argv[2][g] != '0')
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
 		{
 			isn2 = 0;
 			break;
 		}
-
 	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
 		exit(0);
 	}
 }
-
 /**
  * _initialize_array - set memery to zero in a new array
  * @ar: char array.
@@ -43,14 +41,13 @@ void _is_zero(char *argv[])
  */
 char *_initialize_array(char *ar, int lar)
 {
-	int g = 0;
+	int i = 0;
 
-	for (g = 0; g < lar; g++)
-		ar[g] = '0';
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
 	ar[lar] = '\0';
 	return (ar);
 }
-
 /**
  * _checknum - determines length of the number
  * and checks if number is in base 10.
@@ -69,10 +66,8 @@ int _checknum(char *argv[], int n)
 			printf("Error\n");
 			exit(98);
 		}
-
 	return (ln);
 }
-
 /**
  * main - Entry point.
  * program that multiplies two positive numbers.
@@ -83,7 +78,7 @@ int _checknum(char *argv[], int n)
  */
 int main(int argc, char *argv[])
 {
-	int ln1, ln2, lnout, add, addl, g, j, k, ca;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
 	char *nout;
 
 	if (argc != 3)
@@ -93,10 +88,10 @@ int main(int argc, char *argv[])
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);
-	k = lnout - 1, g = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-	for (; k >= 0; k--, g--)
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
 	{
-		if (g < 0)
+		if (i < 0)
 		{
 			if (addl > 0)
 			{
@@ -105,7 +100,7 @@ int main(int argc, char *argv[])
 					nout[k - 1] = (add / 10) + '0';
 				nout[k] = (add % 10) + '0';
 			}
-			g = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
 		}
 		if (j < 0)
 		{
@@ -113,11 +108,11 @@ int main(int argc, char *argv[])
 				break;
 			lnout--;
 			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, g = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
 		if (j >= 0)
 		{
-			add = ((argv[1][g] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
 			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
