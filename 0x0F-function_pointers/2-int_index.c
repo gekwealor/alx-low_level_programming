f9dc90b664face7a52807256ba2a6583cf3dbd75
gekwealor <gekwealor@gmail.com>
2023-05-24 06:57:12 -0700

    Circumstances

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,15 +10,15 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int g;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	for (g = 0; g < size; g++)
 	{
-		if (cmp(array[i]))
-			return (i);
+		if (cmp(array[g]))
+			return (g);
 	}
 	return (-1);
 }
