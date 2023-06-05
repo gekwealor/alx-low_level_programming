13c82cff4487c69533f5cff728dd07e79344536c
gekwealor <gekwealor@gmail.com>
2023-06-05 10:33:06 -0700

    Print da list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - A function that prints all the elements of a linked list
+ * print_listint - Prints all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
