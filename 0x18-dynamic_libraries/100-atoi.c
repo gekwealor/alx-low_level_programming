4245c973d9b45cd9a215b025215634563d243b8d
gekwealor <gekwealor@gmail.com>
2023-07-24 05:33:34 -0700

    Lib dyn

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int a, b, c, len, e, digit;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	len = 0;
+	e = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (a < len && e == 0)
+	{
+		if (s[a] == '-')
+			++b;
+
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			digit = s[a] - '0';
+			if (b % 2)
+				digit = -digit;
+			c = c * 10 + digit;
+			e = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			e = 0;
+		}
+		a++;
+	}
+
+	if (e == 0)
+		return (0);
+
+	return (c);
+}
+
+
---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int g = 0;
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+			{
+				g++;
+				break;
+			}
+			else if (accept[k + 1] == '\0')
+				return (g);
+		}
+		s++;
+	}
+	return (g);
+}
